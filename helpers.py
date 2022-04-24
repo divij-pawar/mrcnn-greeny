1288b4e7a3827fa5eeb1f666b7bc190f05cb1e17
divij-pawar <divijpawar9@gmail.com>
2022-04-24 19:16:18 +0530

    first commit

---
+++ helpers.py
@@ -0,0 +1,64 @@
+#######################################################################
+
+#Apply mask to image
+def apply_mask(image_c, mask, color, alpha=1):
+    """Apply the given mask to the image.
+    """
+    for c in range(3):
+        image_c[:, :, c] = np.where(mask == 1,
+                                  image_c[:, :, c],
+                                  image_c[:, :, c] *
+                                  (1 - alpha) + alpha * color[c] * 255)
+    return image_c
+    
+#######################################################################
+
+#Add two masks together
+def add_mask(current_mask, mask=None):
+  if mask is None:
+    print("Returned")
+    return current_mask
+
+  assert current_mask.shape == mask.shape
+  mask = np.logical_or(mask,current_mask)
+
+  print(mask.shape)
+  return mask
+  
+#######################################################################
+
+# This function is used to show the object detection result in original image.
+def display_instances(image_c, boxes, masks, ids, names, scores):
+
+    # n_instances saves the amount of all objects
+    n_instances = boxes.shape[0]
+
+    if not n_instances:
+        print('NO INSTANCES TO DISPLAY')
+    else:
+        # Check if number of boxes,masks,ids is same
+        assert boxes.shape[0] == masks.shape[-1] == ids.shape[0]
+    mask = None
+
+    for i in range(n_instances):
+        if not np.any(boxes[i]):
+            continue
+
+        # use label to select person object from all the 80 classes in COCO dataset
+        label = names[ids[i]]
+        if label == 'person':
+            # save the largest object in the image as main character
+            # other people will be regarded as background
+            current_mask = masks[:, :, i]
+
+            mask = add_mask(current_mask, mask)
+        else:
+            continue
+
+        # apply mask for the image
+    # by mistake you put apply_mask inside for loop or you can write continue in if also
+
+    image_c = apply_mask(image_c, mask, (0.0,1.0,0.0))
+    return image_c
+    
+    
---
+++ run.py
@@ -0,0 +1,87 @@
+import cv2
+import numpy as np
+import os
+import sys
+import coco
+from mrcnn import utils
+from mrcnn import model as modellib
+from helpers import apply_mask, add_mask, display_instances
+
+py_ver = sys.version_info[0:2]
+if  py_ver >= (3, 9) or py_ver < (3,5) :
+    raise Exception('Requires python between 3.8 and 3.5')
+
+# Load the pre-trained model data
+ROOT_DIR = os.getcwd()
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+    
+    
+# Change the config infermation
+class InferenceConfig(coco.CocoConfig):
+    GPU_COUNT = 1
+    
+    # Number of images to train with on each GPU. A 12GB GPU can typically
+    # handle 2 images of 1024x1024px.
+    # Adjust based on your GPU memory and image sizes. Use the highest
+    # number that your GPU can handle for best performance.
+    IMAGES_PER_GPU = 1
+    
+config = InferenceConfig()
+
+
+# COCO dataset object names
+model = modellib.MaskRCNN(
+    mode="inference", model_dir=MODEL_DIR, config=config
+)
+model.load_weights(COCO_MODEL_PATH, by_name=True)
+class_names = [
+    'BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+    'bus', 'train', 'truck', 'boat', 'traffic light',
+    'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+    'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+    'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+    'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+    'kite', 'baseball bat', 'baseball glove', 'skateboard',
+    'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+    'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+    'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+    'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+    'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+    'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+    'teddy bear', 'hair drier', 'toothbrush'
+]
+
+video_name = input("Enter video filename: ")
+capture = cv2.VideoCapture(video_name)
+output_file = video_name[:-4]+"_output.mp4" 
+# these 2 lines can be removed if you dont have a 1080p camera.
+# capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+# capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+# Recording Video
+fps = round(capture.get(cv2.CAP_PROP_FPS))
+width = int(capture.get(3))
+height = int(capture.get(4))
+out = cv2.VideoWriter(output_file, cv2.VideoWriter_fourcc(*'MP4V'), fps, (width, height))
+
+while True:
+    ret, frame = capture.read()
+    results = model.detect([frame], verbose=0)
+    print("Done predicting")
+    r = results[0]
+    frame = display_instances(
+        frame, r['rois'], r['masks'], r['class_ids'], class_names, r['scores']
+    )
+    cv2.imshow(frame)
+    # Recording Video
+    out.write(frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
